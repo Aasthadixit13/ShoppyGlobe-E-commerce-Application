eeb9a6c6bd6c8c43d243a8a6000d3e23b460e529
Aastha Dixit <dixitaastha13@gmail.com>
2025-11-21 12:33:17 +0530

    feat: update Home.jsx - use Redux filtered products & remove old props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,15 +9,37 @@
 // export default Home;
 
 
+// import React from "react";
+// import ProductList from "../components/ProductList";
+// import { products } from "../data/products";
+
+// function Home({ cart, setCart }) {
+//   return (
+//     <div style={{ padding: "2rem", textAlign: "center" }}>
+//       <h1>Our Products</h1>
+//       <ProductList products={products} cart={cart} setCart={setCart} />
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
+// src/pages/Home.jsx
 import React from "react";
 import ProductList from "../components/ProductList";
-import { products } from "../data/products";
+import { useSelector } from "react-redux";
+import { selectFilteredProducts } from "../store/selectors";
+import SearchBar from "../components/SearchBar"; // Bonus: add search here
 
-function Home({ cart, setCart }) {
+function Home() {
+  const products = useSelector(selectFilteredProducts); // From Redux + search
+
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
       <h1>Our Products</h1>
-      <ProductList products={products} cart={cart} setCart={setCart} />
+      <SearchBar />
+      <ProductList products={products} /> {/* No props like cart/setCart anymore */}
     </div>
   );
 }
