441baebb9d8ed8e133f03b4f127682abdaf0523e
Aastha Dixit <dixitaastha13@gmail.com>
2025-11-21 11:27:05 +0530

    Add props and PropTypes to CartItem for reusability

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+// import React from "react";
+
+// function CartItem({ item, cart, setCart }) {
+//   const removeItem = () => {
+//     setCart(cart.filter((i) => i.id !== item.id));
+//   };
+
+//   const updateQuantity = (e) => {
+//     const updatedCart = cart.map((i) => i.id === item.id ? { ...i, quantity: Number(e.target.value) } : i);
+//     setCart(updatedCart);
+//   };
+
+//   return (
+//     <div>
+//       <h4>{item.name}</h4>
+//       <p>₹{item.price}</p>
+//       <input type="number" value={item.quantity} onChange={updateQuantity} min="1" />
+//       <button onClick={removeItem}>Remove</button>
+//     </div>
+//   );
+// }
+
+// export default CartItem;
+
+
+import PropTypes from "prop-types";
 
 function CartItem({ item, cart, setCart }) {
-  const removeItem = () => {
-    setCart(cart.filter((i) => i.id !== item.id));
-  };
-
-  const updateQuantity = (e) => {
-    const updatedCart = cart.map((i) => i.id === item.id ? { ...i, quantity: Number(e.target.value) } : i);
-    setCart(updatedCart);
-  };
-
-  return (
-    <div>
-      <h4>{item.name}</h4>
-      <p>₹{item.price}</p>
-      <input type="number" value={item.quantity} onChange={updateQuantity} min="1" />
-      <button onClick={removeItem}>Remove</button>
-    </div>
-  );
+  // removeItem and updateQuantity logic
 }
+
+CartItem.propTypes = {
+  item: PropTypes.object.isRequired,
+  cart: PropTypes.array.isRequired,
+  setCart: PropTypes.func.isRequired,
+};
 
 export default CartItem;
