dad0c2b8a75dbc5eea35e0c073042f6baf170dde
Aastha Dixit <dixitaastha13@gmail.com>
2025-11-22 14:03:06 +0530

    resolve the redux error from the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 
 // src/App.jsx
 import React, { Suspense } from "react";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 // import { store } from "./store";
 import store from './store/store.js'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
