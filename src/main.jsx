120bb994df7224daf1278cc7314937d80b04a785
Aastha Dixit <dixitaastha13@gmail.com>
2025-12-01 15:43:16 +0530

    fix: remove .js extension from store import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'          
-import store from './store/store.js'     
+// import store from './store/store.js'     
+import store from './store/store'  
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
